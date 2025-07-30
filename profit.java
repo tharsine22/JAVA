825f5870d532324728d24fc46cdc100bccc790a0
tharsine22 <vntharsine22052006@gmail.com>
2025-07-30 21:50:34 +0530

    Create profit.java

---
+++ profit.java
@@ -0,0 +1,10 @@
+class main {
+    public static void main (String[] args){
+        int a=1000;
+        int b=2;
+        int c=1;
+        int d=100;
+        int e=(b-c)*a-d;
+        System.out.println("profit:" + e);
+    }
+}
