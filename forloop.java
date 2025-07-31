0da6412df2b8002487c45c3e800d67e7056ac021
tharsine22 <vntharsine22052006@gmail.com>
2025-07-31 10:38:02 +0530

    Create forloop.java

---
+++ forloop.java
@@ -0,0 +1,9 @@
+public class forloop {
+    public static void main(String[] args) {
+        int b=0;
+        for (int i=0;i<=100;i++){
+        System.out.println(b);
+        b++;
+        }
+    }
+}
