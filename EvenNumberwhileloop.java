5a178a804a20232fae7abcf6e19ff9b8c54f4448
tharsine22 <vntharsine22052006@gmail.com>
2025-07-31 11:27:41 +0530

    Create EvenNumberwhileloop.java

---
+++ EvenNumberwhileloop.java
@@ -0,0 +1,11 @@
+public class EvenNumberwhileloop {
+public static void main(String[] args) {
+    int n=1;
+    while(n<=50){
+        if(n%2==0){
+        System.out.print(n+" ");
+        }
+        n++;
+    }
+    }
+}
