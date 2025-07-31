c8a25befcac10703ab7cb6a1dab6d754d0993486
tharsine22 <vntharsine22052006@gmail.com>
2025-07-31 11:50:21 +0530

    Create SumFrom1TO50.java

---
+++ SumFrom1TO50.java
@@ -0,0 +1,11 @@
+public class SumFrom1TO50 {
+public static void main(String[] args) {
+    int a=1;
+    int n=1;
+    while (a<=50){
+    n+=a;
+    a++;
+    }
+    System.out.print("sum:"+ n);
+}
+}
