eaa073eebd67b3f944fb2a3b82a8a74e330c2496
tharsine22 <vntharsine22052006@gmail.com>
2025-08-04 09:02:28 +0530

    Create Largest number in array.java

---
+++ Largest number in array.java
@@ -0,0 +1,12 @@
+public class LargestInArray {
+    public static void main(String[] args) {
+        int[] numbers = {10,70,50,40};
+        int largest = numbers[0];  
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > largest) {
+                largest = numbers[i]; 
+            }
+        }
+        System.out.println("The largest number in the array is: " + largest);
+    }
+}
