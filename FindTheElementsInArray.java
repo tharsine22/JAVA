f715cab1b1e0bbb286ac893553a1aa3cd6a211ef
tharsine22 <vntharsine22052006@gmail.com>
2025-08-04 10:11:16 +0530

    Update FindTheElementsInArray.java

--- FindTheElementsInArray.java
+++ FindTheElementsInArray.java
@@ -1,11 +1,21 @@
-public class FindTheElementsInArray{
-    public static void main(String[] args){
-        int[] num={10,20,30,40};
-        int sum = num[0];  
-        for (int i = 1; i < num.length; i++) {
-            if(num[i]==30){
-                System.out.print(i+ "- " + num[i]);
+import java.util.Scanner;
+public class FindElementInArray {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40};
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter element to search: ");
+        int key = sc.nextInt();
+        boolean found = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                System.out.println(i + "- " + arr[i]);
+                found = true;
+                break;
             }
         }
+        if (!found) {
+            System.out.println("Not Found");
+        }
+        sc.close();
     }
 }
